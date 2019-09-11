957b112dbff5904fd328805b91c827d809812e9e
BTomiHun <53236154+BTomiHun@users.noreply.github.com>
2019-09-11 11:15:29 +0200

    Update state.h

--- cellular_automata/c_code/state.h
+++ cellular_automata/c_code/state.h
@@ -4,10 +4,12 @@
 union Number32bit
 {
     // TODO
-    // what is c?
+    // what is c? 
+    // c is the crystallographic orientation
 	unsigned char c[4];
     // TODO
     // what is l?
+    // array for the orientation (last 8 but is not used)
 	unsigned long l;
 };
 
@@ -15,11 +17,16 @@ struct State
 {
     // TODO
     // please write down what each of these vars represent
+	//P is for the phase, binary
+	//D is deformed, binary
     // please write down the type of these vars - what values can they assume?
+	
     // phase, this seems to be binary
     int P;
+	
     // deformed, this seems to be binary
     int D;
+	
     // orientation, this seems to be continuous
     Number32bit O;
 };

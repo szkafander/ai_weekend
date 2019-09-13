b16d0c0cb3cd08b82dcb35d705d7effca968c1f5
ai4i <toth.pala@gmail.com>
2019-09-13 23:26:30 +0200

    added some methods to reimplement

--- cellular_automata/python_reimplementation.py
+++ cellular_automata/python_reimplementation.py
@@ -5,9 +5,10 @@ Created on Thu Sep 12 23:41:05 2019
 @author: tothp
 """
 import numpy as np
-from scipy import signal
 
-from typing import Optional
+from scipy import signal
+from tabulate import tabulate
+from typing import Optional, Tuple, Union
 
 
 dirichlet_kernel = np.array([[0, 1, 0],
@@ -21,24 +22,75 @@ neumann_kernel = np.array([[1, 1, 1],
 default_kernel = dirichlet_kernel
 
 
-def material_props_from_file(filename: str) -> dict:
-    """ Reads mat props from file and returns a dict. """
+# methods related to reading material properties
+# -----------------------------------------------------------------------------
+# these are the category names
+CATS = ("Material", "PHASE0", "PHASE1", "Process", "Structure")
+
+def _clear_string(string: str) -> str:
+    return string.strip().replace("[", "").replace("]", "")
+
+
+def _parse_line(line: str) -> Tuple:
+    return _clear_string(line).split("\t")
+
+
+def read_constants(filename: str) -> dict:
+    """ Reads constant props into nested dict {category: {property: value}}. 
+    The text file should be properly formatted. The first line should be a 
+    category name. """
+    constants = {}
+    f = open(filename)
+    for k, line in enumerate(f):
+        parsed = _parse_line(line)
+        n = len(parsed)
+        if n == 1:
+            category, = parsed
+            if category not in constants:
+                constants[category] = {}
+        elif n == 2:
+            if k == 0:
+                raise ValueError("The first line should be a category.")
+            prop, value = parsed
+            constants[category].update({prop: value})
+        else:
+            raise ValueError("Invalid parsed line {}".format(parsed))
+    return constants
+
+
+# methods related to updating probabilities
+# -----------------------------------------------------------------------------
+# these are the order of property names for vectorized Arrhenius rate calc
+MATERIAL_PROPS = ("Tall", "Qallg", "Ballg", "Estor")
+PHASE_PROPS = ("Qrexn", "Brexn", "Qrexg", "Brexg", "Qc", "Qalln", "Balln",
+               "Gg", "Kg")
+STRUCT_PROPS = ("q", "grain", "def", "phase")
+PROCESS_PROPS = ("W", "H", "proj", "step", "samp", "term", "T")
+
+def arrhenius(
+        temperature: Union[float, np.ndarray],
+        activation_energy: Union[float, np.ndarray],
+        pre_exponential: Union[float, np.ndarray] = 1.0
+    ) -> np.ndarray:
+    """ Calculates an Arrhenius-form rate given T, E and A. If any of these is 
+    a numpy vector, all other vectors should have the same shape. T should be
+    in K. """
+    return pre_exponential * np.exp(- activation_energy / 8.314 / temperature)
+
+
+def probabilities(temperature: float) -> dict:
+    """ Pull mat props into np array, apply Arrhenius formula vectorized, then 
+    fill probabilities dict. """
     pass
 
 
-def probabilities(temperature: float) -> None:
-    """ Pull mat props into np array, apply Arrhenius formula vectorized, then fill
-    probabilities dict. """
-    pass
-
-
-def enforce_boundaries(field: np.ndarray) -> np.ndarray:
+# methods that act on fields
+# -----------------------------------------------------------------------------
+def enforce_boundary_condition(field: np.ndarray) -> np.ndarray:
     """ Enforces the periodic BC on field and returns a new field. """
     return np.pad(field, 1, 'wrap')
 
 
-# methods that act on binary fields
-# -----------------------------------------------------------------------------
 def count_live_neighbors(
         field: np.ndarray,
         kernel: Optional[np.ndarray] = None,
